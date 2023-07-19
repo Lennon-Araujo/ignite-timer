8d5013d1ffa41295e25f310a96f4b233c9fc82c8
Lennon Araujo <lennon_hih@hotmail.com>
2023-07-19 18:35:19 -0300

    improving TaskInput

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,7 +7,19 @@ export function Home() {
       <form action="#">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput type="text" id="task" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput
+            type="text"
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+            <option value="Projeto 4"></option>
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
