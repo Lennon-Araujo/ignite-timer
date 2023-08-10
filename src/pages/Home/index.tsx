a6cc87791d4dcf24932ee19c2cdeeb64adf5aa12
Lennon Araujo <lennon_hih@hotmail.com>
2023-08-10 20:20:16 -0300

    implementing newCycle logic

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -12,6 +12,7 @@ import {
   StartCountdownButton,
   TaskInput
 } from "./styles";
+import { useState } from "react";
 
 /*
 React Hook Form
@@ -49,7 +50,16 @@ Para usar um type (normalmente usado pra extrair um tipo de outro local)
 para integrar ao zod e aproveitar a extração de tipo direto da lib.
 */
 
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+}
+
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
   const { register, handleSubmit, watch, reset } = useForm<FormValues>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -58,12 +68,23 @@ export function Home() {
     }
   });
 
-
   function handleCreateNewCycle(data: FormValues) {
-    console.log(data);
-    reset();
+    const id = String(new Date().getTime());
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount
+    }
+
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+
+    reset()
   }
 
+  const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
+  console.log(activeCycle);
+  
   const task = watch('task');
   const isSubmitDisabled = !task;
 
