0df442a973072044e14416bd54eecff1f4315d64
Lennon Araujo <lennon_hih@hotmail.com>
2023-08-15 19:13:58 -0300

    resolve reset from react hook form

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -66,8 +66,6 @@ export function CyclesContextProvider({ children }: CyclesContextPropType) {
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
     setAmountSecondsPassed(0)
-
-    // reset()
   }
 
   function interruptCurrentCycle() {
